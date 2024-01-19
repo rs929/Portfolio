18e2f87545bd6be3e4be53491cc6ef8a8be3a79f
Richie Sun <rs929@cornell.edu>
2024-01-19 00:38:25 -0500

    Finished about me section

--- sunr-portfolio/src/components/NavBar.js
+++ sunr-portfolio/src/components/NavBar.js
@@ -1,119 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components'
 import GlobalStyle from '../styles/GlobalStyle';
 import { MdClose, MdMenu } from 'react-icons/md'
-
-const NavBarStyle = styled.div`
-    position: fixed;
-    z-index: 100;
-    top: 0;
-    left: 0;
-    width: 100%;
-    padding: 1rem 0;
-    display: flex;
-
-    img {
-        width: 50px;
-        height: auto;
-        margin-left: 1rem;
-    }
-
-    ul {
-        max-width: 1200px;
-        margin: 0 auto;
-        width: 90%;
-        text-align: right;
-
-        li {
-            display: inline-block;
-            border-radius: 10px;
-            transition: .5s ease background-color;
-            &:hover {
-                background-color: var(--hover);
-            }
-        }
-
-        a {
-            display: inline-block;
-            font-family: 'Georgia';
-            padding: 1rem 1rem;
-            font-size: 1.5rem;
-            color: var(--offblack);
-            outline: none;
-        }
-
-        .active {
-            color: var(--purple);
-        }
-    }
-
-    .mobile-menu-icon {
-        position: absolute;
-        right: 1rem;
-        top: 2rem;
-        width: 2rem;
-        margin: 0 auto;
-        cursor: pointer;
-        outline: none;
-        display: none;
-    }
-
-    .close-menu-icon {
-        display: none;
-    }
-
-    @media only screen and (max-width: 770px) {
-        .mobile-menu-icon {
-            display: block;
-        }
-
-        .hide-items { 
-            transform: translateX(150%);
-        }
-
-        .nav-items {
-            transition: .5s ease transform;
-            background-color: white;
-            outline: auto;
-            outline-color: var(--purple);
-            padding: 1rem;
-            width: 35%;
-            max-width: 300px;
-            border-radius: 10px;
-            position: absolute;
-            right: 1rem;
-            top: 1rem;
-
-            .close-menu-icon {
-                display: block;
-                width: 3rem;
-                margin: 0 0 0 auto;
-                cursor: pointer;
-
-                * {
-                    pointer-events: none;
-                }
-            }
-
-            li {
-                display: block;
-                margin-bottom: 1rem;
-                text-align: center;
-            }
-        }
-
-        
-
-    }
-`;
+import './NavBar.css'
 
 
 export default function NavBar() {
-    const [showNav, SetShowNav] = useState(false )
+    const [showNav, SetShowNav] = useState(false)
+    const [sticky, setSticky] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setSticky(window.scrollY > 50);
+        }
+        
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll)
+    })
 
     return (
-        <NavBarStyle>
+        <nav className={`${sticky ? "navBar sticky" : "navBar"}` }>
             <a href="/">
                 <img src='rslogo.svg'></img>
             </a>
@@ -139,6 +46,6 @@ export default function NavBar() {
                     <NavLink to="/contact" onClick = {() => SetShowNav(!showNav)} role='button' onKeyDown={()=>SetShowNav(!showNav)} tabIndex={0}>Contact</NavLink>
                 </li>
             </ul>
-        </NavBarStyle>
+        </nav>
     )
 }
--- sunr-portfolio/src/pages/Home.js
+++ sunr-portfolio/src/pages/Home.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import Hero from '../components/hero/hero'
+import Header from '../components/textSection/textSection';
+import Hero from '../components/hero/hero';
 import NavBar from '../components/NavBar';
+import TextSection from '../components/textSection/textSection';
 
 export default function Home() {
     return (
         <div>
             <Hero />
+            <TextSection/>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
         </div>
     )
 }
